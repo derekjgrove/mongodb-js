e6365451c01b992cd7539bbbf97e340a74dfa189
Derek Grove <derekjgrove@gmail.com>
2022-03-10 23:19:23 -0500

    script changes

--- deployment-scripts/release-1.0.js
+++ deployment-scripts/release-1.0.js
@@ -1,4 +1,4 @@
-client = connect("mongodb://cluster0-shard-00-00.xzkla.mongodb.net:27017,cluster0-shard-00-01.xzkla.mongodb.net:27017,cluster0-shard-00-02.xzkla.mongodb.net:27017/admin?replicaSet=atlas-t3jo8a-shard-0", "testUser ", "testUser ")
+client = Mongo("mongodb://cluster0-shard-00-00.xzkla.mongodb.net:27017,cluster0-shard-00-01.xzkla.mongodb.net:27017,cluster0-shard-00-02.xzkla.mongodb.net:27017/?replicaSet=atlas-t3jo8a-shard-0", "testUser ", "testUser ")
 db = client.getDB('sample_training')
 
 printjson(db.getCollection('companies').find({name: 'Wetpaint'}))
