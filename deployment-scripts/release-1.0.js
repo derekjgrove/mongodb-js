c5519bda295164263e6443bd02d4f357cde6ac50
Derek Grove <derekjgrove@gmail.com>
2022-03-10 22:59:39 -0500

    updates to release script

--- deployment-scripts/release-1.0.js
+++ deployment-scripts/release-1.0.js
@@ -1,4 +1,5 @@
-client = Mongo('mongodb+srv://daragies:daragies@mongo-content-env-stg.dvbfw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+client = connect("mongo-content-env-stg.dvbfw.mongodb.net/myFirstDatabase", "daragies", "daragies")
 db = client.getDB('db-content-services')
 
 printjson(db.getCollection('channels').find({_id: 'channel:mcp:callsign:Z002'}, {title: 1}))
+db.getCollection('channels').find({_id: 'channel:mcp:callsign:Z002'}, {title: 1})
