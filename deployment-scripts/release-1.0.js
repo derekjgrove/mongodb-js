f701409cda20e3a8c63b632254733c0da99e4ea6
Derek Grove <derekjgrove@gmail.com>
2022-03-10 23:33:44 -0500

    script changes

--- deployment-scripts/release-1.0.js
+++ deployment-scripts/release-1.0.js
@@ -1,5 +1,5 @@
-client = Mongo("mongodb://cluster0-shard-00-00.xzkla.mongodb.net:27017,cluster0-shard-00-01.xzkla.mongodb.net:27017,cluster0-shard-00-02.xzkla.mongodb.net:27017/?replicaSet=atlas-t3jo8a-shard-0", "testUser ", "testUser ")
-db = client.getDB('sample_training')
+//client = Mongo("mongodb://cluster0-shard-00-00.xzkla.mongodb.net:27017,cluster0-shard-00-01.xzkla.mongodb.net:27017,cluster0-shard-00-02.xzkla.mongodb.net:27017/?replicaSet=atlas-t3jo8a-shard-0", "testUser ", "testUser ")
+//db = client.getDB('sample_training')
 
-printjson(db.getCollection('companies').find({name: 'Wetpaint'}))
-db.getCollection('companies').find({name: 'Wetpaint'})
+//printjson(db.getCollection('companies').find({name: 'Wetpaint'}))
+print(db.companies.find({name: 'Wetpaint'}))
